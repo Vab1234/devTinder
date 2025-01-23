c8b1a989eb3c843c04833a38c19c5194dd2545f5
Vab1234 <varunbudhani1954@gmail.com>
2025-01-23 20:56:35 +0530

    changes

--- src/app.js
+++ src/app.js
@@ -20,6 +20,11 @@ app.use("/test" , (req , res) => {
     res.send("You are in test bitch")
 });
 
+app.use("/", (req , res) => {
+    res.send("Namaste varun ji")
+});
+// if we use the above code as the first route in our routes sequence then it will override all other routes
+
 app.listen(3000 , () => {
     console.log("Server is running on port 3000...")
 })
